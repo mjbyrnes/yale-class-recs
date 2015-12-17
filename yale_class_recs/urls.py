753b1b38d28f6e6a02a1c2e303d867116ab7e97a
hhclark4 <howard.clark@yale.edu>
2015-12-17 01:42:57 -0500

    added ability to change password

--- yale_class_recs/urls.py
+++ yale_class_recs/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout, password_change, password_change_done
 from . import views
 
 urlpatterns = [
@@ -9,6 +9,13 @@ urlpatterns = [
 		{'next':'/yale_class_recs/accounts/profile'}}, name='login'),
 	url(r'^logout/$', logout,
 		{'template_name': 'registration/logout.html', 'extra_context': {}}, name='logout'),
+	url(r'^password_change/$', password_change,
+		{'template_name': 'registration/password_change.html',
+		'post_change_redirect': 'yale_class_recs:password_change_done', 'extra_context': {}},
+		name='password_change'),
+	url(r'^password_change/done/$', password_change_done,
+		{'template_name': 'registration/password_change_done.html', 'extra_context': {}},
+		name='password_change_done'),
 	url(r'^new_user_info/$', views.get_new_user_info, name='new_user_info'),
 	url(r'^accounts/profile/$', views.user_home, name='user_home_page'),
 	url(r'^accounts/edit_info/$', views.edit_info, name="edit_info"),
