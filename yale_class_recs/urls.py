694dfcf25543a7144df4651bd408437a3b741437
hhclark4 <howard.clark@yale.edu>
2015-12-16 21:51:21 -0500

    added ability to edit basic user information; logout functionality; rearranged nav bars

--- yale_class_recs/urls.py
+++ yale_class_recs/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.CoverView.as_view(), name='cover'),
-	url('^', include('django.contrib.auth.urls')),
+	url(r'^login$', 'django.contrib.auth.views.login',
+		{'template_name': 'registration/login.html', 'extra_context': {'next':'accounts/profile'}}),
+	url(r'^logout$', 'django.contrib.auth.views.logout',
+		{'template_name': 'registration/logout.html', 'extra_context': {}}),
 	url(r'^new_user_info$', views.get_new_user_info, name='new_user_info'),
-	url(r'^home$', views.user_home, name='user_home_page'),
+	url(r'^accounts/profile$', views.user_home, name='user_home_page'),
+	url(r'^accounts/edit_info$', views.edit_info, name="edit_info"),
 	url(r'^info$', views.get_name, name='info'),
 ]
--- yale_class_recs/views.py
+++ yale_class_recs/views.py
@@ -8,8 +8,8 @@ from .forms import UserForm, StudentForm, CourseForm
 
 from .models import Student
 
-def index(request):
-    return HttpResponse(index.html)
+#def index(request):
+#    return HttpResponse(index.html)
 
 class CoverView(generic.ListView):
   template_name = "yale_class_recs/cover.html"
@@ -17,23 +17,15 @@ class CoverView(generic.ListView):
     return None
 
 def get_name(request):
-  # if this is a POST request we need to process the form data
   if request.method == 'POST':
-      # create a form instance and populate it with data from the request:
-      form = StudentForm(request.POST)
-      # check whether it's valid:
+      form = CourseForm(request.POST)
       if form.is_valid():
-          # process the data in form.cleaned_data as required
-          # ...
-          # redirect to a new URL:
+
           return HttpResponseRedirect('/thanks/')
-
-  # if a GET (or any other method) we'll create a blank form
   else:
-      form = StudentForm()
-      form2 = CourseForm()
+      form = CourseForm()
 
-  return render(request, 'yale_class_recs/info.html', {'form': form,'form2': form2})
+  return render(request, 'yale_class_recs/info.html', {'form': form,})
 
 def get_new_user_info(request):
   error = 0
@@ -68,7 +60,7 @@ def get_new_user_info(request):
         if user is not None:
           if user.is_active:
             login(request, user)
-            return HttpResponseRedirect('/yale_class_recs/user/' + username)
+            return HttpResponseRedirect('/yale_class_recs/account/profile')
           else:
             error = "An account for this user already exists, but it is deactivated"
         else:
@@ -90,8 +82,36 @@ def get_new_user_info(request):
 def user_home(request):
   if not request.user.is_authenticated():
     return HttpResponseRedirect('/yale_class_recs/login')
+
+  student = Student.objects.get(user=request.user)
   context = {
+    'first_name': student.first_name,
+    'last_name': student.last_name,
+    'grad_year': student.grad_year,
+    'major': student.major,
     'username': request.user.username,
   }
   return render(request, 'yale_class_recs/user_home_page.html', context)
 
+def edit_info(request):
+  if not request.user.is_authenticated():
+    return HttpResponseRedirect('/yale_class_recs/login')
+
+  student = Student.objects.get(user=request.user)
+
+  if request.method == 'POST':
+    form = StudentForm(request.POST)
+    if form.is_valid():
+      student.first_name = form.cleaned_data['first_name']
+      student.last_name = form.cleaned_data['last_name']
+      student.grad_year = form.cleaned_data['grad_year']
+      student.major = form.cleaned_data['major']
+      student.save()
+    return HttpResponseRedirect('/yale_class_recs/accounts/profile')
+
+  form = StudentForm({'first_name': student.first_name, 'last_name': student.last_name,
+    'grad_year': student.grad_year, 'major': student.major})
+  context = {
+    'form': form,
+  }
+  return render(request, 'yale_class_recs/edit_info.html', context)
