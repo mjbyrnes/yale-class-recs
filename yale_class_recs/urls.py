ec2499b1e5f7eb4a3831a84ee157af4da8b31906
hhclark4 <howard.clark@yale.edu>
2015-12-16 23:16:43 -0500

    changed url and login path

--- yale_class_recs/urls.py
+++ yale_class_recs/urls.py
@@ -4,12 +4,13 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.CoverView.as_view(), name='cover'),
-	url(r'^login$', 'django.contrib.auth.views.login',
-		{'template_name': 'registration/login.html', 'extra_context': {'next':'accounts/profile'}}),
-	url(r'^logout$', 'django.contrib.auth.views.logout',
+	url(r'^login/$', 'django.contrib.auth.views.login',
+		{'template_name': 'registration/login.html', 'extra_context': 
+		{'next':'/yale_class_recs/accounts/profile'}}),
+	url(r'^logout/$', 'django.contrib.auth.views.logout',
 		{'template_name': 'registration/logout.html', 'extra_context': {}}),
-	url(r'^new_user_info$', views.get_new_user_info, name='new_user_info'),
-	url(r'^accounts/profile$', views.user_home, name='user_home_page'),
-	url(r'^accounts/edit_info$', views.edit_info, name="edit_info"),
-	url(r'^info$', views.get_name, name='info'),
+	url(r'^new_user_info/$', views.get_new_user_info, name='new_user_info'),
+	url(r'^accounts/profile/$', views.user_home, name='user_home_page'),
+	url(r'^accounts/edit_info/$', views.edit_info, name="edit_info"),
+	url(r'^info/$', views.get_name, name='info'),
 ]
